aced6346339ac195be2e0bf0d48ea082a3225973
CyberLiTech <136077064+0xCyberLiTech@users.noreply.github.com>
2023-06-15 20:13:06 +0200

    Update install-portainer.sh

--- install-portainer.sh
+++ install-portainer.sh
@@ -9,7 +9,6 @@
 # chmod +x install-portainer.sh
 # sudo ./install-portainer.sh
 # --------------------------------------------------------
-
 sudo docker run -d -p 9000:9000 --name portainer \
     --restart=always \
     -v /var/run/docker.sock:/var/run/docker.sock \
