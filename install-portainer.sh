253bc54a142b266fa5fa391f8d167b59a62fa87f
CyberLiTech <136077064+0xCyberLiTech@users.noreply.github.com>
2023-06-15 17:21:51 +0200

    Update install-portainer.sh

--- install-portainer.sh
+++ install-portainer.sh
@@ -3,7 +3,7 @@
 # --------------------------------------------------------
 # CyberLiTech
 # Script créé le 09-06-2023 
-# Script modifié le 09-06-2023
+# Script modifié le 15-06-2023
 #
 # touch install-portainer.sh
 # chmod +x install-portainer.sh
