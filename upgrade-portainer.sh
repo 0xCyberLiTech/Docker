7b62f395d4ef2c8919d7c1aabefc633f786a694a
CyberLiTech <136077064+0xCyberLiTech@users.noreply.github.com>
2023-06-15 19:21:57 +0200

    Update upgrade-portainer.sh

--- upgrade-portainer.sh
+++ upgrade-portainer.sh
@@ -10,17 +10,6 @@
 # sudo ./upgrade-portainer.sh
 # --------------------------------------------------------
 
-# Mise à jour automatisée de Portainer sur DEBIAN 11.x
-# --------------------------------------------------------
-# 
-
-# Portainer permettant de mettre à jour facilement nos conteneurs, il peut être tentant de l’utiliser également pour le mettre à jour lui-même. 
-# Spoiler : c’est une mauvaise idée qui va tout simplement casser votre Portainer. 
-
-# Nous allons tout d’abord arrêter le conteneur puis le supprimer. 
-# Comme nous utilisons un volume, les données importantes du conteneur ne seront pas supprimées.
-# On télécharge ensuite la dernière image de Portainer, puis nous relançons le conteneur avec les mêmes réglages qu’à l’origine.
-
 sudo docker stop portainer
 sudo docker rm portainer
 sudo docker pull portainer/portainer-ce:latest
@@ -31,4 +20,3 @@ sudo docker run -d -p 9000:9000 --name portainer \
     -v portainer_data:/data \
     portainer/portainer-ce:latest
 
-# Portainer est maintenant à jour et tous ses réglages ont été conservés.
