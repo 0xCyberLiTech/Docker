7580d1a61ba8d478959d241b6f51f770cffc668d
CyberLiTech <136077064+0xCyberLiTech@users.noreply.github.com>
2023-07-08 09:18:38 +0200

    Update installation-automatisée-de-docker-compose.sh

--- installation-automatisée-de-docker-compose.sh
+++ installation-automatisée-de-docker-compose.sh
@@ -10,14 +10,15 @@
 # sudo ./installation-automatisée-de-docker-compose.sh
 # --------------------------------------------------------
 
+# Récupération de la dernière version de docker-compose disponible :
 COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
 
-# Install docker-compose
+# Installation de docker-compose dans sa dernière version :
 sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
 chmod +x /usr/local/bin/docker-compose
 sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
 
-# Output compose version
+# Vérification de la dernière version docker-compose installée :
 docker-compose -v
 
 exit 0
