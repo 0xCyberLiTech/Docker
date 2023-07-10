9baf53c80e1069e0c496bcd873ac5a65123f8ab9
CyberLiTech <136077064+0xCyberLiTech@users.noreply.github.com>
2023-07-10 14:59:10 +0200

    Update installation-automatisée-de-docker-compose.sh

--- installation-automatisée-de-docker-compose.sh
+++ installation-automatisée-de-docker-compose.sh
@@ -3,22 +3,18 @@
 # --------------------------------------------------------
 # 0xCyberLiTech
 # Script créé le 08-07-2023
-# Script modifié le 08-07-2023
+# Script modifié le 10-07-2023
 #
 # touch installation-automatisée-de-docker-compose.sh
 # chmod +x installation-automatisée-de-docker-compose.sh
 # sh ./installation-automatisée-de-docker-compose.sh
 # --------------------------------------------------------
-
-# Récupération de la dernière version de docker-compose disponible :
-COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
-
-# Installation de docker-compose dans sa dernière version :
-sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
-chmod +x /usr/local/bin/docker-compose
-sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
-
-# Vérification de la dernière version docker-compose installée :
-docker-compose -v
-
+sudo apt update
+sudo apt install -y curl wget
+curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url  | grep docker-compose-linux-x86_64 | cut -d '"' -f 4 | wget -qi -
+chmod +x docker-compose-linux-x86_64
+sudo mv docker-compose-linux-x86_64 /usr/local/bin/docker-compose
+docker-compose version
+sudo usermod -aG docker $USER
+newgrp docker
 exit 0
